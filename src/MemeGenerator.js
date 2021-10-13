9c83d68fad10bbae8cece315c55ee99a45e0448f
OpeDada <opedada@gmail.com>
2021-10-13 22:51:08 +0900

    completed meme generator

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -6,13 +6,61 @@ class MemeGenerator extends React.Component {
       this.state = {
           topText: "",
           bottomText: "",
-          randomImg: "http://i.imgflip.com/1bij.jpg"
+          randomImg: "http://i.imgflip.com/1bij.jpg",
+          allMemeImgs: []
       }
+    }
+
+      componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(res => {
+              const {memes} = res.data
+              this.setState({ allMemeImgs: memes })
+            })
+          this.handleChange = this.handleChange.bind(this)
+          this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+      handleChange(event) {
+        const {name, value} = event.target
+        this.setState({ [name]: value })
+    }
+
+    handleSubmit(event) {
+      event.preventDefault()
+      const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+      const randMemeImg = this.state.allMemeImgs[randNum].url
+      this.setState({ randomImg: randMemeImg })
     }
 
   render(){
     return (
-      <h1>MEME SECTION</h1>
+      <div>
+        <form className="meme-form" onSubmit={this.handleSubmit}>
+          <input
+              type="text"
+              name="topText"
+              placeholder="Top text"
+              value={this.state.topText}
+              onChange={this.handleChange}
+          />
+          <input
+              type="text"
+              name="bottomText"
+              placeholder="Bottom text"
+              value={this.state.bottomText}
+              onChange={this.handleChange}
+          />
+            <button>Gen</button>
+        </form>
+        <div className="meme">
+          <img src={this.state.randomImg} alt=""/>
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+
+        </div>
+      </div>
     )
   }
 }
